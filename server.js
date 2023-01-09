16bc4a8ab1a3d7e7db86e3684cb945e49f3626bb
Athirson <athirsonarceus@gmail.com>
2023-01-09 19:06:58 -0300

    try catch

--- server.js
+++ server.js
@@ -13,41 +13,49 @@ app.use('/', router)
 
 app.listen(5000, () => console.log('Server Running'))
 
-const contactEmail = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'athirsonarceus@gmail.com',
-		pass: 'eyyvwyheilecpabw'
-	}
-})
-
-contactEmail.verify((error) => {
-	if (error) {
-		console.log(error)
-	}
-})
-
-router.post('/contact', (request, response) => {
-	const { firstName, lastName, email, phone, message } = request.body
-
-	const mail = {
-		from: email,
-		to: 'athirsonarceus@gmail.com',
-		subject: 'Contact Form Submission - Portfolio',
-		html: `<p>Name: ${firstName} ${lastName}</p>
-           <p>From: ${email} - ${phone}</p>
-           ${message}`
-	}
-
-	contactEmail.sendMail(mail, (error) => {
-		if (error) {
-			response.json(error)
-		} else {
-			response.json({
-				code: 200,
-				status: 'Message Sent Successfully!',
-				mail
-			})
+try {
+	const contactEmail = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'athirsonarceus@gmail.com',
+			pass: 'eyyvwyheilecpabw'
 		}
 	})
+
+	contactEmail.verify((error) => {
+		if (error) {
+			console.log(error)
+		}
+	})
+} catch (error) {
+	console.log(error)
+}
+
+router.post('/contact', (request, response) => {
+	try {
+		const { firstName, lastName, email, phone, message } = request.body
+
+		const mail = {
+			from: email,
+			to: 'athirsonarceus@gmail.com',
+			subject: 'Contact Form Submission - Portfolio',
+			html: `<p>Name: ${firstName} ${lastName}</p>
+           <p>From: ${email} - ${phone}</p>
+           ${message}`
+		}
+
+		contactEmail.sendMail(mail, (error) => {
+			if (error) {
+				response.json(error)
+			} else {
+				response.json({
+					code: 200,
+					status: 'Message Sent Successfully!',
+					mail
+				})
+			}
+		})
+	} catch (error) {
+		console.log(error)
+	}
 })
